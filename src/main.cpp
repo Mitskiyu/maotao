84457cb77625b6bada075abd6ba8c690427e0f6f
Denzel <denzel.mitski@gmail.com>
2025-03-27 14:26:31 +0100

    feat: add grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <string>
+#include <vector>
 
 constexpr int windowWidth = 1024;
 constexpr int windowHeight = 768;
@@ -70,6 +71,65 @@ class Menu
 };
 
 //
+// :grid
+//
+class Grid
+{
+  public:
+    Grid(int rows, int cols)
+        : rows(rows), cols(cols), cellSize((int)(float(windowHeight) * 0.9f / rows)),
+          // calc offset to center grid
+          offset({static_cast<float>((windowWidth - cols * cellSize)) / 2,
+                  static_cast<float>((windowHeight - rows * cellSize)) / 2})
+    {
+        cells.resize(rows, std::vector<int>(cols, 0));
+    }
+
+    void draw() const
+    {
+        for (int row = 0; row < rows; ++row)
+        {
+            for (int col = 0; col < cols; ++col)
+            {
+                // calc cell to draw
+                float x = offset.x + col * cellSize;
+                float y = offset.y + row * cellSize;
+
+                if (cells[row][col] == 1)
+                {
+                    DrawRectangle(x, y, cellSize + 1, cellSize + 1, GREEN);
+                }
+
+                DrawRectangleLinesEx({x, y, (float)cellSize, (float)cellSize}, 1, GRAY);
+            }
+        }
+    }
+
+    void update()
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            Vector2 mousePos = GetMousePosition();
+            int col = (mousePos.x - offset.x) / cellSize;
+            int row = (mousePos.y - offset.y) / cellSize;
+
+            if (row >= 0 && row < rows && col >= 0 && col < cols)
+            {
+                // toggle state
+                cells[row][col] = 1 - cells[row][col];
+            }
+        }
+    }
+
+  private:
+    const int rows;
+    const int cols;
+    const int cellSize;
+    Vector2 offset;
+    std::vector<std::vector<int>> cells;
+};
+
+//
 // :main
 //
 int main()
@@ -77,14 +137,19 @@ int main()
     InitWindow(windowWidth, windowHeight, title.c_str());
 
     Menu menu;
+    Grid grid(16, 16);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
         ClearBackground(BLACK);
+
         menu.update();
         menu.draw();
+
+        grid.update();
+        grid.draw();
 
         EndDrawing();
     }
