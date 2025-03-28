f9a3055673499a47e99dd2e465846ef281338604
Denzel <denzel.mitski@gmail.com>
2025-03-28 08:50:29 +0100

    feat: add simulation

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 enum GameState
 {
     MENU,
-    GAME
+    GAME,
+    PAUSED
 };
 
 constexpr int windowWidth = 1024;
@@ -91,6 +92,26 @@ class Grid
         cells.resize(rows, std::vector<int>(cols, 0));
     }
 
+    int getRows() const
+    {
+        return rows;
+    }
+
+    int getCols() const
+    {
+        return cols;
+    }
+
+    int getCell(int row, int col) const
+    {
+        return cells[row][col];
+    }
+
+    void setCell(int row, int col, int val)
+    {
+        cells[row][col] = val;
+    }
+
     void draw() const
     {
         for (int row = 0; row < rows; ++row)
@@ -136,17 +157,100 @@ class Grid
 };
 
 //
+// :sim
+//
+class Simulation
+{
+  public:
+    Simulation(Grid &grid, GameState &currentState)
+        : accumulator(0.0f), tickrate(0.1f), grid(grid), currentState(currentState),
+
+          // possible neighbors
+          neighbors({{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}})
+    {
+    }
+
+    void update(float dt)
+    {
+        if (currentState != GAME)
+        {
+            return;
+        }
+
+        accumulator += dt;
+
+        if (accumulator >= tickrate)
+        {
+            accumulator = 0.0f;
+
+            // temp grid
+            std::vector<std::vector<int>> tempGrid(grid.getRows(), std::vector<int>(grid.getCols(), 0));
+
+            for (int row = 0; row < grid.getRows(); ++row)
+            {
+                for (int col = 0; col < grid.getCols(); ++col)
+                {
+                    int activeNeighbors = 0;
+
+                    // determine active neighbors
+                    for (const auto &[dr, dc] : neighbors)
+                    {
+                        int r = row + dr;
+                        int c = col + dc;
+                        if (r >= 0 && r < grid.getRows() && c >= 0 && c < grid.getCols())
+                        {
+                            activeNeighbors += grid.getCell(r, c);
+                        }
+                    }
+
+                    int currentCell = grid.getCell(row, col);
+
+                    if (currentCell == 1)
+                    {
+                        // any live cell with 2 or 3 live neighbors lives, otherwise die
+                        tempGrid[row][col] = (activeNeighbors == 2 || activeNeighbors == 3) ? 1 : 0;
+                    }
+                    else
+                    {
+                        // any dead cell with exactly 3 neighbors becomes alive
+                        tempGrid[row][col] = (activeNeighbors == 3) ? 1 : 0;
+                    }
+                }
+            }
+
+            // set cell status in grid
+            for (int row = 0; row < grid.getRows(); ++row)
+            {
+                for (int col = 0; col < grid.getCols(); ++col)
+                {
+                    grid.setCell(row, col, tempGrid[row][col]);
+                }
+            }
+        }
+    }
+
+  private:
+    float accumulator;
+    float tickrate;
+    Grid &grid;
+    GameState &currentState;
+    std::vector<std::pair<int, int>> neighbors;
+};
+
+//
 // :main
 //
 int main()
 {
     InitWindow(windowWidth, windowHeight, title.c_str());
-
-    Menu menu;
-    Grid grid(16, 16);
+    SetTargetFPS(120);
 
     // track game state
     GameState currentState = MENU;
+
+    Menu menu;
+    Grid grid(32, 32);
+    Simulation sim(grid, currentState);
 
     while (!WindowShouldClose())
     {
@@ -161,8 +265,25 @@ int main()
             menu.draw();
             break;
         case GAME:
+            sim.update(GetFrameTime());
             grid.update();
             grid.draw();
+
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                currentState = (currentState == GAME) ? PAUSED : GAME;
+            }
+
+            break;
+        case PAUSED:
+            grid.update();
+            grid.draw();
+
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                currentState = GAME;
+            }
+
             break;
         }
 
